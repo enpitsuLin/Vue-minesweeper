82d0a971680f6022da5bdf7e377de91667dbbb87
unknown <lldslm@vip.qq.com>
2019-03-28 13:49:55 +0800

    update readme.md and deltte some code

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-#HTML5Mine
-# HTML5Mine
+#HTML5Mine HTMl5扫雷
+
+本项目是于学习jQuery时产出的一个小游戏。
+
+所用技术栈
+>Javascript,JQuery,html,css
+
+以后所接触的技术栈会逐渐往该项目上延伸。
+
+现存目标
+>You-Dont-Need-jQuery 抛弃jQuery
+>Do with Reactjs or Vuejs
+>Better style
+>MORE
--- js/MinesSweeper.js
+++ js/MinesSweeper.js
@@ -324,31 +324,6 @@ Minesweeper.prototype._initEvents = function() {
 		}
 	);
 
-	$('#mineBlock').on({  
-		touchstart: function(e) { 
-			// 长按事件触发  
-			timeOutEvent = setTimeout(function() {  
-				timeOutEvent = 0;  
-				alert('你长按了');  
-			}, 400);  
-			//长按400毫秒   
-			// e.preventDefault();    
-		},  
-		touchmove: function() {  
-			clearTimeout(timeOutEvent);  
-			timeOutEvent = 0;  
-		},  
-		touchend: function() {  
-			clearTimeout(timeOutEvent);  
-			if (timeOutEvent != 0) {  
-				// 点击事件  
-				// location.href = '/a/live-rooms.html';  
-				alert('你点击了');  
-			}  
-			return false;  
-		}  
-	}) 
-
 	$(window).resize(function() {
 		minesweeper._initMineSize();
 	});
