dce2626115c55fbd57562bed7f962d08b2bcf69d
unknown <lldslm@vip.qq.com>
2019-04-06 18:18:10 +0800

    remake by vue with vue-cli

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>{{ message }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello Vue.js!"
+    };
+  }
+};
+</script>
+
+<style lang="less">
+</style>
